759f55887270b6889c2b3615383295356c15da2c
moinakash <moinakashll@gmail.com>
2020-02-24 15:33:01 +0600

    Filter Button Fix

--- app/src/main/java/com/example/camerabeautify/Activity/CameraWithFilterActivity.java
+++ app/src/main/java/com/example/camerabeautify/Activity/CameraWithFilterActivity.java
@@ -225,6 +225,7 @@ public class CameraWithFilterActivity extends Activity implements  View.OnClickL
     private void more_button(){
 
 
+        onTouchLayout.setVisibility(View.GONE);
         llt_flash_col.setVisibility(View.GONE);
         mMenuView.setVisibility(View.GONE);
 
@@ -361,6 +362,8 @@ public class CameraWithFilterActivity extends Activity implements  View.OnClickL
             // ----- Flash column -----
             case R.id.img_main_flash:
 
+                onTouchLayout.setVisibility(View.GONE);
+
                 //TODO
                 llt_layout_more.setVisibility(View.GONE);
 
@@ -448,6 +451,7 @@ public class CameraWithFilterActivity extends Activity implements  View.OnClickL
 
             case R.id.btn_sticker:
 
+                onTouchLayout.setVisibility(View.GONE);
                 mFilterLayout.setVisibility(View.GONE);
                 llt_face_col.setVisibility(View.GONE);
                 llt_face_seek.setVisibility(View.GONE);
@@ -576,6 +580,7 @@ public class CameraWithFilterActivity extends Activity implements  View.OnClickL
 
     private void showFaceBeauty(){
 
+        onTouchLayout.setVisibility(View.GONE);
         mFilterLayout.setVisibility(View.GONE);
         mMenuView.setVisibility(View.GONE);
 
@@ -1084,6 +1089,7 @@ public class CameraWithFilterActivity extends Activity implements  View.OnClickL
 
     }
 
+    //todo
     private void showFilters(){
         ObjectAnimator animator = ObjectAnimator.ofFloat(mFilterLayout, "translationY", mFilterLayout.getHeight(), 0);
         animator.setDuration(200);
@@ -1091,7 +1097,9 @@ public class CameraWithFilterActivity extends Activity implements  View.OnClickL
 
             @Override
             public void onAnimationStart(Animator animation) {
-                findViewById(R.id.btn_camera_shutter).setClickable(false);
+                findViewById(R.id.btn_camera_shutter).setClickable(true);
+
+                onTouchLayout.setVisibility(View.GONE);
                 mFilterLayout.setVisibility(View.VISIBLE);
 
                 llt_face_col.setVisibility(View.GONE);
