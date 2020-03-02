5944e811c87bffaafea69383a3755eea98f891bc
moinakash <moinakashll@gmail.com>
2020-03-02 12:01:53 +0600

    6 sticker added

--- app/src/main/java/com/example/camerabeautify/Activity/CameraWithFilterActivity.java
+++ app/src/main/java/com/example/camerabeautify/Activity/CameraWithFilterActivity.java
@@ -1097,6 +1097,12 @@ public class CameraWithFilterActivity extends Activity implements  View.OnClickL
         bean.image = R.drawable.st_gamcha;
         mStickerData.add(bean);
 
+        bean=new MenuBean();
+        bean.name="";
+        bean.path="pinkribon";
+        bean.image = R.drawable.st_gamcha;
+        mStickerData.add(bean);
+
 
         mStickerAdapter.notifyDataSetChanged();
     }
