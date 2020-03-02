c31961f7751a9a0e51590c0921e5f7153ed5b121
farhadhossen <imfarhad01@gmail.com>
2020-03-02 12:51:25 +0600

    kaner dul

--- app/src/main/java/com/example/camerabeautify/Activity/CameraWithFilterActivity.java
+++ app/src/main/java/com/example/camerabeautify/Activity/CameraWithFilterActivity.java
@@ -1109,6 +1109,12 @@ public class CameraWithFilterActivity extends Activity implements  View.OnClickL
         bean.image = R.drawable.st_gamcha;
         mStickerData.add(bean);
 
+        bean=new MenuBean();
+        bean.name="";
+        bean.path="kanerdul";
+        bean.image = R.drawable.st_gamcha;
+        mStickerData.add(bean);
+
 
         mStickerAdapter.notifyDataSetChanged();
     }
