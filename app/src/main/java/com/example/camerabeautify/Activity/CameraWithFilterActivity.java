506955958a5ba75247aa6d737d71b79f6ce9fa46
fahimGUB <asif.iqbal.fahim.bd@gmail.com>
2020-03-02 16:24:36 +0600

    scrf

--- app/src/main/java/com/example/camerabeautify/Activity/CameraWithFilterActivity.java
+++ app/src/main/java/com/example/camerabeautify/Activity/CameraWithFilterActivity.java
@@ -1161,6 +1161,12 @@ public class CameraWithFilterActivity extends Activity implements  View.OnClickL
         bean.image = R.drawable.st_gamcha;
         mStickerData.add(bean);
 
+        bean=new MenuBean();
+        bean.name="";
+        bean.path="scrf";
+        bean.image = R.drawable.st_gamcha;
+        mStickerData.add(bean);
+
 
 
         mStickerAdapter.notifyDataSetChanged();
