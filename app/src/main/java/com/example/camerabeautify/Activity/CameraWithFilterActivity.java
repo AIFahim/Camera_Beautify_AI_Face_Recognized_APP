371c0d339de8fc985cab0d3467bc9269faadd2b8
farhadhossen <imfarhad01@gmail.com>
2020-03-02 16:37:47 +0600

    hairtwo

--- app/src/main/java/com/example/camerabeautify/Activity/CameraWithFilterActivity.java
+++ app/src/main/java/com/example/camerabeautify/Activity/CameraWithFilterActivity.java
@@ -1167,6 +1167,12 @@ public class CameraWithFilterActivity extends Activity implements  View.OnClickL
         bean.image = R.drawable.st_gamcha;
         mStickerData.add(bean);
 
+        bean=new MenuBean();
+        bean.name="";
+        bean.path="hairtwo";
+        bean.image = R.drawable.st_gamcha;
+        mStickerData.add(bean);
+
 
 
         mStickerAdapter.notifyDataSetChanged();
